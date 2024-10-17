8aee2dd3b27fe92ce245574a4fad98fc02e0662d
Santiago-School <a.23308051140421@cbtis128.edu.mx>
2024-10-17 15:43:18 -0600

    Add files via upload

---
+++ practica4-2o-parcial.py
@@ -0,0 +1,29 @@
+print("Francisco Santiago Carrasco Correa 0421 3°W")
+print(" ")
+# se crea un diccionario vacio
+diccionario = {}
+
+# se pide el nombre y se añade al diccionario
+nombre = input("ingresa tu nombre: ")
+diccionario["nombre"] = nombre
+print(diccionario)
+
+# se pide la edad y se añade al diccionario
+edad = input("ingresa tu edad: ")
+diccionario["edad"] = edad
+print(diccionario)
+
+# se pide el sexo y se añade al diccionario
+sexo = input("ingresa tu sexo: ")
+diccionario["sexo"] = sexo
+print(diccionario)
+
+# se pide el telefono y se añade al diccionario
+telefono = input("ingresa tu telefono: ")
+diccionario["telefono"] = telefono
+print(diccionario)
+
+# se pide el correo electronico y se añade al diccionario
+correo = input("ingresa tu correo electronico: ")
+diccionario["correo"] = correo
+print(diccionario)
